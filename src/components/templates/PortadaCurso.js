24dd9a6ac5f543f0e880d92e28f779dcdf1797f7
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-09-26 13:53:41 -0500

    Areglando bugs de los Navs

--- src/components/templates/PortadaCurso.js
+++ src/components/templates/PortadaCurso.js
@@ -5,20 +5,24 @@ const Wrapper = styled.div`
   background-size:cover;
   margin-top:2em;
   width:calc(100vw - 20vw);
-  height:180px;
+  height:190px;
   border-radius:8px;
-  @media (min-width:660px){
-    height:240px;
+  @media (min-width:715px){
+    height:235px;
   }
-  @media (max-width:660px){
+  @media (max-width:715px){
     background-size:cover;
     background-repeat:no-repeat;
+    width:calc(100vw - 10vw);
   }
 `;
 const TitleCurso = styled.h1`
     color:#fff;
   font-size:28px;
   padding:1em;
+    font-weight: 600;
+    line-height: 2.25rem;
+    letter-spacing:2px;
 `;
 
 const PortadaCurso = ({data}) => {
